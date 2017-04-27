669bb03c9bd1cb8f3729344de06ec051a65f648e
Francesco Filogamo <assistenza.dotnet@gmail.com>
2017-04-27 15:05:42 +0200

    Initial commit

---
+++ app/src/main/java/it/ornet/demo/MainActivity.java
@@ -0,0 +1,13 @@
+package it.ornet.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
